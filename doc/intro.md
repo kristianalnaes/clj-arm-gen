b2ef2078826e2339f294df06f2fd1136df603518
Kristian Alnaes <kristianalnaes@gmail.com>
2017-10-03 17:00:46 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to armgen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/armgen/core_test.clj
@@ -0,0 +1,7 @@
+(ns armgen.core-test
+  (:require [clojure.test :refer :all]
+            [armgen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
